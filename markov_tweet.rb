2a2e390a75d38fd0bf98687a5409b8ed2f993e0c
issei126 <issei.nakamura@peroli.jp>
2017-05-07 22:04:34 +0900

    add an argument

--- markov_tweet.rb
+++ markov_tweet.rb
@@ -2,7 +2,7 @@ require("#{File.dirname(File.expand_path(__FILE__))}/Bottou.rb")
 require("#{File.dirname(File.expand_path(__FILE__))}/twitter_client.rb")
 
 tw_rest_client = TwitterClient.rest_client
-markov = Markov.new
+markov = Markov.new(tw_rest_client)
 b = Bottou.new(tw_rest_client)
 
-b.markov_tweet(markov) if rand(100) < 30
+b.markov_tweet(markov) if rand(100) < 30 # 30％の確率でつぶやくように。
