9830ce4a5a1c94ffc65b620ff8396a6b2544f021
issei126 <issei.nakamura@peroli.jp>
2017-05-25 22:14:34 +0900

    fix reply

--- Bottou_reply.rb
+++ Bottou_reply.rb
@@ -1,4 +1,8 @@
 require("#{File.dirname(File.expand_path(__FILE__))}/Bottou.rb")
+require("#{File.dirname(File.expand_path(__FILE__))}/twitter_client.rb")
+
+tw_rest_client = TwitterClient.rest_client
+b = Bottou.new(tw_rest_client)
 
 b = Bottou.new
 b.reply
