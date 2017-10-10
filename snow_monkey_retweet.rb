2d1ae41b5f14bb4350388e326246f913dd8b9fea
issei126 <issei.nakamura@peroli.jp>
2017-10-10 22:42:56 +0900

    add snow_monkey_retweet

---
+++ snow_monkey_retweet.rb
@@ -0,0 +1,12 @@
+require("#{File.dirname(File.expand_path(__FILE__))}/twitter_client.rb")
+
+class SnowMonkeyRetweet
+  THE_TWEET_ID = 246252965302775809
+
+  def self.run
+    tw_rest_client = TwitterClient.rest_client
+    tw_rest_client.unretweet(THE_TWEET_ID)
+    tw_rest_client.retweet(THE_TWEET_ID)
+  end
+
+end
