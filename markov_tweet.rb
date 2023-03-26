15169f8a09ed81eaacb184fb4df6fc28d1f048e1
issei126 <isseeei@gmail.com>
2023-03-26 10:50:06 +0900

    output time

--- markov_tweet.rb
+++ markov_tweet.rb
@@ -1,8 +1,13 @@
 require("#{File.dirname(File.expand_path(__FILE__))}/Bottou.rb")
 require("#{File.dirname(File.expand_path(__FILE__))}/twitter_client_v2.rb")
 
+
+puts "### Start markov at #{Time.now} ###"
+
 tw_rest_client = TwitterClient.new
 markov = Markov.new(tw_rest_client)
 b = Bottou.new(tw_rest_client)
 
 b.markov_tweet(markov) if rand(100) < 30 # 30％の確率でつぶやくように。
+
+puts "### End markov at #{Time.now} ###"
