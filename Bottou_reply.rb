68cba63571ebfb571ae1cc403430bb1295eb55df
issei126 <isseeei@gmail.com>
2023-03-26 10:53:09 +0900

    change log

--- Bottou_reply.rb
+++ Bottou_reply.rb
@@ -1,11 +1,11 @@
 require("#{File.dirname(File.expand_path(__FILE__))}/Bottou.rb")
 require("#{File.dirname(File.expand_path(__FILE__))}/twitter_client_v2.rb")
 
-puts "### Start markov at #{Time.now} ###"
+puts "### Start reply at #{Time.now} ###"
 
 tw_rest_client = TwitterClient.new
 b = Bottou.new(tw_rest_client)
 
 b.reply
 
-puts "### End markov at #{Time.now} ###"
+puts "### End reply at #{Time.now} ###"
